e5c063bfbfe51093506611d516097fbbcdbedebe
Utkarsh <utkarsh.harsh2002@gmail.com>
2023-07-08 01:23:00 +0530

    please

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL : "https://mern-chat-app-pr5w.onrender.com/",
+    baseURL : "https://mern-chat-app-pr5w.onrender.com",
 });
